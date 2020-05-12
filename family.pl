173504bdd93d7cc0752ec2909ddb2598938dddea
student <oreillyj@email.sc.edu>
2020-05-12 08:56:51 -0700

    likes and family Examples

--- family.pl
+++ family.pl
@@ -6,9 +6,23 @@ child(sue,george).   child(sue,gina).
 
 male(john).   male(sam).     male(george). 
 female(sue).  female(jane).  female(june). 
+female(gina).
 
 parent(Y,X) :- child(X,Y).
 father(Y,X) :- child(X,Y), male(Y).
 opp_sex(X,Y) :- male(X), female(Y). 
 opp_sex(Y,X) :- male(X), female(Y). 
 grand_father(X,Z) :- father(X,Y), parent(Y,Z).
+
+mother(M,C) :- child(C,M), female(M).
+
+sibling(S1,S2):-child(S1,P),child(S2,P),\+ S1=S2.
+
+person(P) :- child(P,_).
+person(P) :- child(_,P).
+
+%all_gendered :- \+ (person(P), \+(male(P);female(P)) ).
+
+ungendered(P) :- person(P),\+male(P),\+female(P).
+
+all_gendered :- \+ ungendered(_).
--- likes.pl
+++ likes.pl
@@ -3,5 +3,22 @@ likes(john,pizza).                   % John likes pizza.
 likes(john,sushi).                   % John likes sushi.
 likes(mary,sushi).                   % Mary likes sushi.
 likes(paul,X) :- likes(john,X).      % Paul likes what John likes.
-likes(X,icecream).                   % Everybody likes ice cream.
+likes(P,icecream) :- person(P).                   % Everybody likes ice cream.
+
+likes(P, calzone) :- likes(P,pizza).
+likes(P,cilantro) :- person(P), \+ P = mary.
+
+person(john).
+person(paul).
+person(mary).
+
+food(pizza).
+food(sushi).
+food(calzone).
+food(icecream).
+food(cilantro).
+
+dislikes(P,F) :- person(P),food(F),\+ likes(P,F).
+
+
 
